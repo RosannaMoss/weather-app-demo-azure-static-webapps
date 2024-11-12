40de1c129bbb741085af8e30039a77aeee8a8323
Rosanna Moss <rosannam@missionreadyhq.com>
2024-11-12 16:36:58 +1300

    personal key added to js api

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const cityInput = document.getElementById("input");
 const dispalySection = document.getElementById("display");
 
-
-const API_KEY = "650ad8fb3c6e4ecab3190253232706";
+const API_KEY = "6d4a67212d4a4a11a50211321242509";
 
 const handleClick = () => {
   let currentCity = cityInput.value;
@@ -14,8 +13,7 @@ const handleClick = () => {
   )
     .then((res) => res.json())
     .then((data) => {
-        dispalySection.innerHTML =
-      ` <p>${data.location.name}'s temperature is ${data.current.temp_c}℃.</p>
+      dispalySection.innerHTML = ` <p>${data.location.name}'s temperature is ${data.current.temp_c}℃.</p>
         <p>It's ${data.current.condition.text}.</p>
         <img src="${data.current.condition.icon}" alt="icon"/>
       `;
